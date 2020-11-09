3c5c5a4d0728f07868702a4946b0e1a782d03efe
MY-PC <vivpur88@gmail.com>
2020-11-09 18:25:06 +0530

    feature: adds median scaling function

--- utility_functions.py
+++ utility_functions.py
@@ -6,7 +6,7 @@
 import numpy as np
 import pandas as pd
 
-def threshold_data(feature):
+def threshold_data(data):
     
     '''
         Apply thresholding to data. Threshold values are decided 
@@ -17,22 +17,38 @@ def threshold_data(feature):
         
         Parameters:
         -----------
-            feature: the range of values to be thresholded.
+            data: the range of values to be thresholded.
             
         Returns:
         -------
         A dictionary containing minimum threshold, maximum  threshold, Thresholded values
         
     '''
-    feature = pd.Series(feature)
+    data = pd.Series(data)
     
-    q1, q3 = np.percentile(feature, [25, 75])
+    q1, q3 = np.percentile(data, [25, 75])
     IQR = q3 - q1
     
     min_threshold = q1 - 1.5*IQR
     max_threshold = q3 + 1.5*IQR
     
-    thresholded_data = feature.apply(lambda x: min(max(min_threshold, x), max_threshold))
+    thresholded_data = data.apply(lambda x: min(max(min_threshold, x), max_threshold))
     
     return {'min_th': min_threshold, 'max_th': max_threshold, 'th_data': thresholded_data}
+    
+
+def median_transformation(variable):
+    '''
+        Z - transformation uses mean and is provided as:
+         z = (x - u) / s
+         
+        However, to use it in presence of outliers, we replace mean statistics with median,
+        and standard deviation with Median Absolute deviation.
+        As below:
+         z = (x - median) / mad
+    '''
+    median = np.median(variable)
+    mad = stats.median_abs_deviation(variable, nan_policy='omit')
+    
+    return pd.Series(variable).apply(lambda x: (x-median)/mad)
     
